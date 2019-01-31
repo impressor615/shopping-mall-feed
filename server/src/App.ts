a063a22a22cac0d66d0074ec3f75f3048fb8a956
seonghyeon <hyeon@apartmentary.com>
2019-02-01 00:26:47 +0900

    server - add compression, bodyParser middleware
    [Why]
    
    * make response to be compressed using compression middleware

--- server/src/App.ts
+++ server/src/App.ts
@@ -1,6 +1,10 @@
 import * as express from "express";
+import * as bodyParser from 'body-parser';
+import * as compression from 'compression';
 
 const app = express();
+app.use(bodyParser.json());
+app.use(compression());
 app.get('/', (req:express.Request, res: express.Response) => {
   res.send('Hello World');
 });
