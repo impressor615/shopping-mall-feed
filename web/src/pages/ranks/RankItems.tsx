9ae9a74ca16ed079bae0b900f390869f9409c502
seonghyeon <hyeon@apartmentary.com>
2019-02-14 02:58:02 +0900

    web - delete react-lazyload
    [Why]
    
    *

--- web/src/pages/ranks/RankItems.tsx
+++ web/src/pages/ranks/RankItems.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import LazyLoad from "react-lazyload";
 
 import RankItem from "@/components/RankItem";
 
@@ -7,20 +6,14 @@ const RankItems = ({ data }: RankState) => (
   <>
     {
       data.map((item, index) => (
-        <LazyLoad
-          once
-          height={200}
-          offset={100}
+        <RankItem
           key={item.id}
-        >
-          <RankItem
-            name={item.name}
-            tags={item.description}
-            imgSrc={item.image.url}
-            url={item.url}
-            rank={index + 1}
-          />
-        </LazyLoad>
+          name={item.name}
+          tags={item.description}
+          imgSrc={item.image.url}
+          url={item.url}
+          rank={index + 1}
+        />
       ))
     }
   </>
