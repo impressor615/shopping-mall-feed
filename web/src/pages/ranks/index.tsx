ca336ec897fdabd858ac3e835b7503b8aebb9a45
seonghyeon <hyeon@apartmentary.com>
2019-02-14 00:28:08 +0900

    web - update ranks page to use api data
    [Why]
    
    *

--- web/src/pages/ranks/index.tsx
+++ web/src/pages/ranks/index.tsx
@@ -1,13 +1,15 @@
 import React from "react";
+import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 
+import { getRanks } from "@/actions";
 import Container from "@/components/Container";
 import Header from "@/components/Header";
-import RANKS from "@/data/rank.json";
 
 import RankItems from "./RankItems";
 
-class Page extends React.PureComponent<RouterProps, {}> {
+interface PageProps extends ReduxRouterProps, RankState {}
+class Page extends React.PureComponent<PageProps, {}> {
   public onFilterClick = (e: React.MouseEvent) => {
     e.stopPropagation();
     const { name }: any = e.target;
@@ -16,14 +18,23 @@ class Page extends React.PureComponent<RouterProps, {}> {
     history.push("/products");
   }
 
+  public async componentDidMount() {
+    const { dispatch } = this.props;
+    await dispatch(getRanks());
+  }
+
   public render() {
+    const { data } = this.props;
     return (
       <Container>
         <Header onClick={this.onFilterClick} filter="ranks" />
-        <RankItems data={RANKS.data} />
+        <RankItems data={data} />
       </Container>
     );
   }
 }
 
-export default withRouter(Page as any);
+const mapStateToProps = (state: StoreState) => ({
+  ...state.rank,
+});
+export default withRouter(connect(mapStateToProps)(Page) as any);
