f479f43de6f105ab076593a37305a5fdf8236aef
seonghyeon <hyeon@apartmentary.com>
2019-02-14 14:21:43 +0900

    server - update router to return data ordered by id
    [Why]
    
    *

--- server/src/routes/product.ts
+++ server/src/routes/product.ts
@@ -5,7 +5,7 @@ import getModels from "@/Database";
 export default (router: express.Router): void => {
   router.get("/products", async (req: express.Request, res: express.Response) => {
     const { Product } = await getModels();
-    const result = await Product.findAll();
+    const result = await Product.findAll({ order: [["id", "DESC"]] });
     res.json({ data: result });
   });
 };
--- server/src/routes/rank.ts
+++ server/src/routes/rank.ts
@@ -5,7 +5,7 @@ import getModels from "@/Database";
 export default (router: express.Router): void => {
   router.get("/ranks", async (req: express.Request, res: express.Response) => {
     const { Rank } = await getModels();
-    const result = await Rank.findAll();
+    const result = await Rank.findAll({ order: [["id", "DESC"]] });
     res.json({ data: result });
   });
 };
